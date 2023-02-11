50d76330415e519d516b108af291d19d8ca667e1
Lks9172 <ekdan9172@naver.com>
2023-02-11 14:29:08 +0900

    env: swagger(api 문서 툴) 적용 완료

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+    .setTitle('Sleact API')
+    .setDescription('Dev sleact docs')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+ 
     await app.listen(3000);
 }
 bootstrap();
