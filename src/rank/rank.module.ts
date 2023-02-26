296b343ff59508c96d2836e46a835a8c2f96323c
Lks9172 <ekdan9172@naver.com>
2023-02-26 23:09:19 +0900

    fix: 사용되지 않는 rank 도메인의 서비스, 컨트롤러 제거

--- src/rank/rank.module.ts
+++ src/rank/rank.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LogDataService } from './application/log-data.service';
-import { LogDataController } from './presentation/log-data.controller';
 import { DataLogTypeRepository } from './repository/commit-log.repository';
 import { LogDataRepository } from './repository/log-data.repository';
 
 @Module({
-  controllers: [LogDataController],
-  providers: [    
-    LogDataService,
-  ],
+  controllers: [],
+  providers: [],
   imports: [
     TypeOrmModule.forFeature([
       LogDataRepository,
