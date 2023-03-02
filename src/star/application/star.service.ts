818c92cd43e1e3c2db79ac6f70e40564afcd1eee
buddy <jang_taehwan@naver.com>
2023-03-02 13:27:23 +0900

    feat: star 추가/삭제 기능 구현

--- src/star/application/star.service.ts
+++ src/star/application/star.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { StarRepository } from '../repository/star.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MemberRepository } from '../../member/repository/member.repository';
+import { Member } from '../../member/domain/member.entity';
 import { StarResponseDto } from '../presentation/dto/star-response.dto';
 
 @Injectable()
@@ -20,12 +21,14 @@ export class StarService {
       where: {
         memberId: member,
       },
+      relations: ['memberId', 'followingId'],
     });
 
     const followers = await this.starRepository.find({
       where: {
         followingId: member,
       },
+      relations: ['memberId', 'followingId'],
     });
 
     return new StarResponseDto(
@@ -33,5 +36,68 @@ export class StarService {
       follows.map((star) => star.followingId.id),
       followers.map((star) => star.memberId.id),
     );
+  }
+
+  public async followMember(
+    member: Member,
+    followTargetId: number,
+  ): Promise<StarResponseDto> {
+    this.validateFollowInfo(member, followTargetId);
+
+    const targetMember = await this.memberRepository.findOneOrThrow(
+      followTargetId,
+    );
+
+    const star = await this.starRepository.findOne({
+      where: {
+        memberId: member,
+        followingId: targetMember,
+      },
+    });
+
+    if (star) {
+      throw new BadRequestException('이미 팔로우된 사용자입니다.');
+    }
+
+    const newStar = await this.starRepository.create({
+      memberId: member,
+      followingId: targetMember,
+    });
+
+    await this.starRepository.save(newStar);
+
+    return this.getStarList(member.id);
+  }
+
+  public async unfollowMember(
+    member: Member,
+    unfollowTargetId: number,
+  ): Promise<StarResponseDto> {
+    this.validateFollowInfo(member, unfollowTargetId);
+
+    const targetMember = await this.memberRepository.findOneOrThrow(
+      unfollowTargetId,
+    );
+
+    const star = await this.starRepository.findOne({
+      where: {
+        memberId: member,
+        followingId: targetMember,
+      },
+    });
+
+    if (!star) {
+      throw new BadRequestException('팔로우 하지 않은 사용자입니다.');
+    }
+
+    await this.starRepository.delete(star);
+
+    return this.getStarList(member.id);
+  }
+
+  private validateFollowInfo(member: Member, followTargetId: number) {
+    if (member.id === followTargetId) {
+      throw new BadRequestException('자신을 팔로우 할 수 없습니다.');
+    }
   }
 }
--- src/star/presentation/star.controller.ts
+++ src/star/presentation/star.controller.ts
@@ -1,12 +1,16 @@
 import {
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
+  Post,
   UseGuards,
 } from '@nestjs/common';
 import { StarService } from '../application/star.service';
 import { AuthGuard } from '@nestjs/passport';
+import { GetMember } from '../../auth/presentation/get-member.decorator';
+import { Member } from '../../member/domain/member.entity';
 
 @Controller('star')
 export class StarController {
@@ -14,7 +18,25 @@ export class StarController {
 
   @UseGuards(AuthGuard('jwt'))
   @Get('/:memberId')
-  public async getStarInfo(@Param(ParseIntPipe) memberId: number) {
+  public async getStarInfo(@Param('memberId', ParseIntPipe) memberId: number) {
     return await this.starService.getStarList(memberId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/:memberId')
+  public async followMember(
+    @GetMember() member: Member,
+    @Param('memberId', ParseIntPipe) memberId: number,
+  ) {
+    return await this.starService.followMember(member, memberId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/:memberId')
+  public async unfollowMember(
+    @GetMember() member: Member,
+    @Param('memberId', ParseIntPipe) memberId: number,
+  ) {
+    return await this.starService.unfollowMember(member, memberId);
   }
 }
